9d01825a882b3f5a5d498017d921e88c9a62e4a0
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2021-01-21 09:52:17 -0800

    add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useQuery } from 'react-query';
+
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+
+// Components
+import Item from './Item/Item';
+import Cart from './Cart/Cart';
+
+// Styles
+import { Wrapper, StyledButton } from './App.styles';
+// Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
 
 function App() {
   return (
